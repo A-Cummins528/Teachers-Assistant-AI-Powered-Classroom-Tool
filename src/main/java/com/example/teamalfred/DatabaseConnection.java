c275dde84e78deee0f4e0b367c0fb2dccef26294
A-Cummins528 <108574142+A-Cummins528@users.noreply.github.com>
2025-04-05 21:31:14 +1000

    added Main class, modified it to create a connection to the database

--- src/main/java/com/example/teamalfred/DatabaseConnection.java
+++ src/main/java/com/example/teamalfred/DatabaseConnection.java
@@ -3,6 +3,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/// This class is responsible for creating a connection to the SQLite database
 public class DatabaseConnection {
     private static Connection instance = null;
 
---
+++ src/main/java/com/example/teamalfred/Main.java
@@ -0,0 +1,9 @@
+package com.example.teamalfred;
+import java.util.List;
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+        Connection connection = DatabaseConnection.getInstance();
+    }
+}
