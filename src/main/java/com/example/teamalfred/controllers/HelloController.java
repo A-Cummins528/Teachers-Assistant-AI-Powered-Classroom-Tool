7e04ad6c1defa3146bcb240b4254d8f83c459a8e
NottTigah <117633708+NottTigah@users.noreply.github.com>
2025-04-10 00:46:16 +1000

    Update to SignUp.fxml, added SignUp FXML director, signup button now works

--- src/main/java/com/example/teamalfred/controllers/HelloController.java
+++ src/main/java/com/example/teamalfred/controllers/HelloController.java
@@ -1,14 +1,18 @@
-package com.example.teamalfred.controllers;
+package com.example.teamalfred;
 
-import com.example.teamalfred.Main;
 import com.example.teamalfred.database.DatabaseUserDAO;
 import com.example.teamalfred.database.IUserDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -44,6 +48,31 @@ public class HelloController {
             failedLogin.setText("All below fields are mandatory.");
         } else {
             failedLogin.setText("Invalid credentials.");
+        }
+    }
+
+    @FXML
+    private void handleSignUp(ActionEvent event) {
+        try {
+            System.out.println("Handling signup...");
+            // Load the Signup.fxml file
+            System.out.println(getClass().getResource("/com/example/teamalfred/LogIn.fxml"));
+
+            FXMLLoader loaderb = new FXMLLoader(getClass().getResource("SignUp.fxml"));
+            Parent root = loaderb.load();
+
+            // Create a new scene with the loaded FXML
+            Scene scene = new Scene(root);
+
+            // Get the current stage
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            // Set the new scene on the stage
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle the exception appropriately
         }
     }
 
