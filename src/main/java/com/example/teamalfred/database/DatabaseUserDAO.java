6fbc6b4da136e661e5360935e9ffe5a47a0ed748
NottTigah <117633708+NottTigah@users.noreply.github.com>
2025-04-09 23:45:07 +1000

    Added username element in table and added DeleteTable method to delete the table (reseting it) - FOR TESTING ONLY

--- src/main/java/com/example/teamalfred/database/DatabaseUserDAO.java
+++ src/main/java/com/example/teamalfred/database/DatabaseUserDAO.java
@@ -17,34 +17,45 @@ public class DatabaseUserDAO implements IUserDAO {
 
     private void insertSampleData() {
         try {
-            System.out.println("howdy");
             Statement clearStatement = connection.createStatement();
             String clearQuery = "DELETE FROM users";
             clearStatement.execute(clearQuery);
             Statement insertStatement = connection.createStatement();
-            String insertQuery = "INSERT INTO users (firstName, lastName, mobile, email, password) VALUES "
-                    + "('Josh', 'Madams', '0412345678', 'josh@madams.com', 'password123'), "
-                    + "('Adam', 'Cummins', '0442115891', 'adams@cummins.com', 'password1234'),"
-                    + "('Philip', 'Mouton', '0433123456', 'philip@mouton.com', 'password12345'),"
-                    + "('Justin', 'Coglan', '0411555999', 'justin@coglan.com', 'password123456'), "
-                    + "('Felix', 'Nguyen', '0422555999', 'felix@nguyen.com', 'password1234567')";
+            String insertQuery = "INSERT INTO users (firstName, lastName, mobile, email, password, username) VALUES "
+                    + "('Josh', 'Madams', '0412345678', 'josh@madams.com', 'password123', 'username1'), "
+                    + "('Adam', 'Cummins', '0442115891', 'adams@cummins.com', 'password1234', 'username2'),"
+                    + "('Philip', 'Mouton', '0433123456', 'philip@mouton.com', 'password12345', 'username3'),"
+                    + "('Justin', 'Coglan', '0411555999', 'justin@coglan.com', 'password123456', 'username4'), "
+                    + "('Felix', 'Nguyen', '0422555999', 'felix@nguyen.com', 'password1234567', 'username5')";
             insertStatement.execute(insertQuery);
 
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
+    private void deleteTable() {
+        try {
+            Statement statement = connection.createStatement();
+            String query = "DROP TABLE users";
+            statement.execute(query);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     private void createTable() {
         try {
             Statement statement = connection.createStatement();
+            deleteTable();
+
             String query = "CREATE TABLE IF NOT EXISTS users ("
                     + "id INTEGER PRIMARY KEY AUTOINCREMENT,"
                     + "firstName VARCHAR NOT NULL,"
                     + "lastName VARCHAR NOT NULL,"
                     + "mobile VARCHAR NOT NULL,"
                     + "email VARCHAR NOT NULL,"
-                    + "password VARCHAR NOT NULL"
+                    + "password VARCHAR NOT NULL,"
+                    + "username VARCHAR NOT NULL UNIQUE"
                     + ")";
             statement.execute(query);
         } catch (Exception e) {
