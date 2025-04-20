c996324c3abce2d827efcc78f89c45739a0517ef
felixnguyen76 <vietanhnguyenha2003@gmail.com>
2025-04-20 17:52:06 +1000

    Basic Dashboard Scene
    Dashboard.fxml with some of the tabs.
    
    Haven't implement any function into this since I have no clue what would be the content we want.

--- src/main/java/com/example/teamalfred/controllers/DashboardController.java
+++ src/main/java/com/example/teamalfred/controllers/DashboardController.java
@@ -1,24 +1,40 @@
 package com.example.teamalfred.controllers;
 
 
-import java.io.IOException;
+import com.example.teamalfred.Main;
+import com.example.teamalfred.database.DatabaseConnection;
+import com.example.teamalfred.database.DatabaseUserDAO;
+import com.example.teamalfred.database.IUserDAO;
+import com.example.teamalfred.database.User;
+import com.example.teamalfred.controllers.switchSceneController;
+import javafx.animation.FadeTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import org.w3c.dom.Text;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Statement;
 
 
 // Controller for dashboard.fxml scene
 public class DashboardController {
     private Label welcomeMessageLabel;
     public DashboardController() throws IOException {
-        System.out.println("Dashboard started");
-        welcomeMessageLabel.setText("");
-
     }
+    private switchSceneController switchScene = new switchSceneController();
+    private Connection connection;
+
+    public DashboardController(Connection connection) {
+        this.connection = DatabaseConnection.getInstance();
+    }
+
 }
